fadafb58ee51f474a0d7f5a843d57cab0300a20a
Feudalord <Feudalord@live.co.uk>
2020-08-28 15:50:14 +0800

    Level - 2, add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,15 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String txt;
+        String[] txt = new String[100];
         Scanner in = new Scanner(System.in);
         System.out.println("Hello Hello! I'm Duke\n" + "What can I do for you?");
+        int i = 0;
         while (true) {
-            txt = in.nextLine();
-            if(txt.equals("bye")){
+            txt[i] = in.nextLine();
+            if(txt[i].equals("bye")){
                 break;
+            }else if (txt[i].equals("list")){
+                for(int x = 0; x < i; x++){
+                    System.out.printf("%d. %s\n", x+1, txt[x]);
+                }
+                continue;
             }
-            System.out.printf("%s\n\n", txt);
+            System.out.printf("%s\n\n", txt[i]);
+            i++;
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
