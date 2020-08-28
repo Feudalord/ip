28b068e2bab2b1ad9c468ad6bc9a71e4fa5e8843
Feudalord <Feudalord@live.co.uk>
2020-08-28 16:46:54 +0800

    Level - 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,17 +12,28 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello Hello! I'm Duke\n" + "What can I do for you?");
         int i = 0;
+        Task[] t = new Task[100];
         while (true) {
             txt[i] = in.nextLine();
             if(txt[i].equals("bye")){
                 break;
             }else if (txt[i].equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int x = 0; x < i; x++){
-                    System.out.printf("%d. %s\n", x+1, txt[x]);
+                    System.out.printf("%d."+ t[x].getStatusIcon() + " %s\n", x+1, txt[x]);
+                }
+                txt[i] = in.nextLine();
+                int num;
+                if (txt[i].contains("done")){
+                    num = Integer.parseInt(txt[i].substring(5));
+                    t[num-1].isDone = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.printf(t[num-1].getStatusIcon() + " %s\n", txt[num-1]);
                 }
                 continue;
             }
             System.out.printf("%s\n\n", txt[i]);
+            t[i] = new Task(txt[i]);
             i++;
         }
         System.out.println("Bye. Hope to see you again soon!");
